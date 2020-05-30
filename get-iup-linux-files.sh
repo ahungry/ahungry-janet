da5eece2be12345b309639af44ebb1b93331f7c6
Matthew Carter <m@ahungry.com>
2020-05-30 00:14:59 -0400

    better file fetch

--- get-iup-linux-files.sh
+++ get-iup-linux-files.sh
@@ -2,24 +2,29 @@
 
 top=$(pwd)
 
-# GNU/Linux files
-mkdir -p deps/linux/iup
-cd deps/linux/iup
+if [[ ! -d deps/linux/iup ]]; then
 
-# .a and .so files
-# wget 'https://sourceforge.net/projects/iup/files/3.28/Linux%20Libraries/iup-3.28_Linux50_64_lib.tar.gz' -O iup.tar.gz
-wget 'https://github.com/ahungry/ahungry-janet/releases/download/0.0.1/iup.tar.gz' -O iup.tar.gz
+    # GNU/Linux files
+    mkdir -p deps/linux/iup
+    cd deps/linux/iup
 
-tar xzvf iup.tar.gz
+    # .a and .so files
+    # wget 'https://sourceforge.net/projects/iup/files/3.28/Linux%20Libraries/iup-3.28_Linux50_64_lib.tar.gz' -O iup.tar.gz
+    wget 'https://github.com/ahungry/ahungry-janet/releases/download/0.0.1/iup.tar.gz' -O iup.tar.gz
+
+    tar xzvf iup.tar.gz
+fi
 
 cd $top
 
-mkdir -p deps/linux/im
-cd deps/linux/im
+if [[ ! -d deps/linux/im ]]; then
+    mkdir -p deps/linux/im
+    cd deps/linux/im
 
-# wget 'https://sourceforge.net/projects/imtoolkit/files/3.13/Linux%20Libraries/im-3.13_Linux415_64_lib.tar.gz' -O im.tar.gz
-wget 'https://github.com/ahungry/ahungry-janet/releases/download/0.0.1/im.tar.gz' -O im.tar.gz
+    # wget 'https://sourceforge.net/projects/imtoolkit/files/3.13/Linux%20Libraries/im-3.13_Linux415_64_lib.tar.gz' -O im.tar.gz
+    wget 'https://github.com/ahungry/ahungry-janet/releases/download/0.0.1/im.tar.gz' -O im.tar.gz
 
-tar xzvf im.tar.gz
+    tar xzvf im.tar.gz
 
-#wget 'https://sourceforge.net/projects/canvasdraw/files/5.12/Linux%20Libraries/cd-5.12_Linux44_64_lib.tar.gz'
+    #wget 'https://sourceforge.net/projects/canvasdraw/files/5.12/Linux%20Libraries/cd-5.12_Linux44_64_lib.tar.gz'
+fi
--- get-iup-windows-files.sh
+++ get-iup-windows-files.sh
@@ -2,30 +2,35 @@
 
 top=$(pwd)
 
+if [[ ! -d deps/win/iup ]]; then
 
-mkdir -p deps/win/iup
-cd deps/win/iup
+    mkdir -p deps/win/iup
+    cd deps/win/iup
 
-# Get the Windows files
+    # Get the Windows files
 
-# .a and .dll files
-# wget 'https://sourceforge.net/projects/iup/files/3.28/Windows%20Libraries/Static/iup-3.28_Win64_mingw6_lib.zip' -O iup.zip
-# wget 'https://github.com/ahungry/ahungry-janet/releases/download/0.0.1/iup.zip' -O iup.zip
-wget 'https://github.com/ahungry/ahungry-janet/releases/download/0.0.1/iup-3.28_Win64_dllw6_lib.zip' -O iup.zip
+    # .a and .dll files
+    # wget 'https://sourceforge.net/projects/iup/files/3.28/Windows%20Libraries/Static/iup-3.28_Win64_mingw6_lib.zip' -O iup.zip
+    # wget 'https://github.com/ahungry/ahungry-janet/releases/download/0.0.1/iup.zip' -O iup.zip
+    wget 'https://github.com/ahungry/ahungry-janet/releases/download/0.0.1/iup-3.28_Win64_dllw6_lib.zip' -O iup.zip
 
-unzip iup.zip
-#wget 'https://sourceforge.net/projects/iup/files/3.28/Windows%20Libraries/Dynamic/iup-3.28_Win64_dllw6_lib.zip'
+    unzip iup.zip
+    #wget 'https://sourceforge.net/projects/iup/files/3.28/Windows%20Libraries/Dynamic/iup-3.28_Win64_dllw6_lib.zip'
+fi
 
 cd $top
 
-mkdir -p deps/win/im
-cd deps/win/im
+if [[ ! -d deps/win/im ]]; then
 
-#wget 'https://sourceforge.net/projects/imtoolkit/files/3.13/Windows%20Libraries/Dynamic/im-3.13_Win64_dllw6_lib.zip' -O im.zip
-wget 'https://github.com/ahungry/ahungry-janet/releases/download/0.0.1/im.zip' -O im.zip
-#wget 'https://sourceforge.net/projects/imtoolkit/files/3.13/Windows%20Libraries/Static/im-3.13_Win64_mingw6_lib.zip'
+    mkdir -p deps/win/im
+    cd deps/win/im
 
-unzip im.zip
+    #wget 'https://sourceforge.net/projects/imtoolkit/files/3.13/Windows%20Libraries/Dynamic/im-3.13_Win64_dllw6_lib.zip' -O im.zip
+    wget 'https://github.com/ahungry/ahungry-janet/releases/download/0.0.1/im.zip' -O im.zip
+    #wget 'https://sourceforge.net/projects/imtoolkit/files/3.13/Windows%20Libraries/Static/im-3.13_Win64_mingw6_lib.zip'
 
-# wget 'https://sourceforge.net/projects/canvasdraw/files/5.12/Windows%20Libraries/Dynamic/cd-5.12_Win64_dllw6_lib.zip'
-# wget 'https://sourceforge.net/projects/canvasdraw/files/5.12/Windows%20Libraries/Static/cd-5.12_Win64_mingw6_lib.zip'
+    unzip im.zip
+
+    # wget 'https://sourceforge.net/projects/canvasdraw/files/5.12/Windows%20Libraries/Dynamic/cd-5.12_Win64_dllw6_lib.zip'
+    # wget 'https://sourceforge.net/projects/canvasdraw/files/5.12/Windows%20Libraries/Static/cd-5.12_Win64_mingw6_lib.zip'
+fi
