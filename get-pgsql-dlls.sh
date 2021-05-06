750e8a266a1fd41124312fad26a04abaf48b5a06
Matthew Carter <m@ahungry.com>
2021-05-05 22:27:08 -0400

    Add missing dll fetch

--- get-pgsql-dlls.sh
+++ get-pgsql-dlls.sh
@@ -3,5 +3,6 @@
 DLL_ROOT=/usr/x86_64-w64-mingw32/bin
 
 cp $DLL_ROOT/libpq.dll ./
+cp $DLL_ROOT/libintl-8.dll ./
 cp $DLL_ROOT/libxml2-2.dll ./
 cp $DLL_ROOT/liblzma-5.dll ./
--- get-pgsql-dlls32.sh
+++ get-pgsql-dlls32.sh
@@ -3,5 +3,6 @@
 DLL_ROOT=/usr/i686-w64-mingw32/bin
 
 cp $DLL_ROOT/libpq.dll ./
+cp $DLL_ROOT/libintl-8.dll ./
 cp $DLL_ROOT/libxml2-2.dll ./
 cp $DLL_ROOT/liblzma-5.dll ./
